e2d9458a82f0b1147cc134aee4d22d70c8f3591c
Wojt3kW <wojtek,wojdylo@wp.pl>
2024-05-23 13:19:32 +0200

    disabled eslint rules

--- TypeScript/001_Variables/001_variable_names.test.ts
+++ TypeScript/001_Variables/001_variable_names.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-magic-numbers */
+/* eslint-disable @typescript-eslint/naming-convention */
 import { between, isBetween } from './001_variable_names';
 import './number.extensions';
 
